f06cf789f62d5e50deb06fc7114af9ba0a82d2a1
BrendonLH <Brendonhampton1988@gmail.com>
2022-09-21 19:10:19 -0700

    initial-setup complete, working on components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
+function CookieStand({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default CookieStand
