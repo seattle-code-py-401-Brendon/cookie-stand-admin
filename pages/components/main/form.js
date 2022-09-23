d2e566b6f6cee1f800a2dff150b5e237ae3e1131
Replit user <>
2022-09-23 21:03:33 +0000

    added more styling

--- pages/components/main/form.js
+++ pages/components/main/form.js
@@ -4,13 +4,13 @@ export default function Form() {
 
   const [formInputs, setFormInputs] = useState({});
   let stringifiedFormInputs = JSON.stringify(formInputs);
-  
+
   const handleChange = (e) => {
     let name = e.target.name;
     const value = e.target.value;
-    setFormInputs(values => ({...values, [name]: value}));
+    setFormInputs(values => ({ ...values, [name]: value }));
   }
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formInputs);
@@ -18,14 +18,14 @@ export default function Form() {
 
 
   return (
-    <section>
-      <form onSubmit={handleSubmit} className="bg-form-color max-w-xl">
+    <section className='flex flex-col mx-auto'>
+      <form onSubmit={handleSubmit} className="bg-form-color w-[50rem] rounded border-solid border-2">
         <h3 className=" text-lg font-semibold text-center pt-5">Create Cookie Stand</h3>
         <div className="">
           <label>Location</label>
-          <input name='location' type='text' value={formInputs.location|| ''} onChange={handleChange} className=""></input>
+          <input name='location' type='text' value={formInputs.location || ''} onChange={handleChange}></input>
         </div>
-        <section className="flex"> 
+        <section className="flex">
           <div className="">
             <label>Minimum Customers Per Hour</label>
             <input type='number' name='minCustomersHr' value={formInputs.minCustHr} onChange={handleChange} className=""></input>
@@ -39,11 +39,12 @@ export default function Form() {
             <input type='number' name='avgCustomersHr' value={formInputs.avgCookieHr} onChange={handleChange} className=""></input>
           </div>
         </section>
-        <input type='submit'/>
+        <input type='submit' />
       </form>
+      <p className='font-semibold'>Data Table Coming Soon!</p>
       <p>{stringifiedFormInputs}</p>
-      
-      
+
+
     </section>
   )
 }
