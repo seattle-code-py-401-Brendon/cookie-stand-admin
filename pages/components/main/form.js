b5a2ae8cc124ff85fb5f418b3ea01f7528bbce4a
Replit user <>
2022-09-23 20:29:23 +0000

    added json object

--- pages/components/main/form.js
+++ pages/components/main/form.js
@@ -1,25 +1,49 @@
-export default function Form(){
-    return (
-        <form className="bg-form-color max-w-xl">
-            <h3 className=" text-lg font-semibold text-center pt-5">Create Cookie Stand</h3>
-            <div className="">
-                <label>Location</label>
-                <input className=""></input>
-            </div>
-            <section className="flex">
-                <div className="">
-                    <label>Minimum Customers Per Hour</label>
-                    <input></input>
-                </div>
-                <div>
-                    <label>Maximum Customers Per Hour</label>
-                    <input></input>
-                </div>
-                <div>
-                    <label>Average Cookies Per Hour</label>
-                    <input></input>
-                </div>
-            </section>
+import React, { useState } from 'react';
+
+export default function Form() {
+
+  const [formInputs, setFormInputs] = useState({});
+  let stringifiedFormInputs = JSON.stringify(formInputs);
+  
+  const handleChange = (e) => {
+    let name = e.target.name;
+    const value = e.target.value;
+    setFormInputs(values => ({...values, [name]: value}));
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formInputs);
+  }
+
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit} className="bg-form-color max-w-xl">
+        <h3 className=" text-lg font-semibold text-center pt-5">Create Cookie Stand</h3>
+        <div className="">
+          <label>Location</label>
+          <input name='location' type='text' value={formInputs.location|| ''} onChange={handleChange} className=""></input>
+        </div>
+        <section className="flex"> 
+          <div className="">
+            <label>Minimum Customers Per Hour</label>
+            <input type='number' name='minCustomersHr' value={formInputs.minCustHr} onChange={handleChange} className=""></input>
+          </div>
+          <div>
+            <label>Maximum Customers Per Hour</label>
+            <input type='number' name='maxCustomersHr' value={formInputs.maxCustHr} onChange={handleChange} className=""></input>
+          </div>
+          <div>
+            <label>Average Cookies Per Hour</label>
+            <input type='number' name='avgCustomersHr' value={formInputs.avgCookieHr} onChange={handleChange} className=""></input>
+          </div>
+        </section>
+        <input type='submit'/>
       </form>
-    )
+      <p>{stringifiedFormInputs}</p>
+      
+      
+    </section>
+  )
 }
--- pages/components/main/main.js
+++ pages/components/main/main.js
@@ -1,11 +1,10 @@
 import Form from './form.js';
 
 export default function Main() {
-    return (
-      <div className='flex flex-col pt-10 '>
-        <Form />
-        <p>Report Table Coming Soon...</p>
-      </div>
-      
-    )
-  }
+  return (
+    <div className='flex flex-col pt-10 '>
+      <Form />
+    </div>
+
+  )
+}
